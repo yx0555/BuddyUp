94dffe927440dc0f82eea6272054446a03c4ffa6
joeyylow <e0564115@u.nus.edu>
2022-03-06 22:24:15 +0800

    Register.vue VER.1: Created basic functionalities of registration page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,107 @@
 <template>
-  <h1>Register Page</h1>
+  <div id="app">
+    <img src="src/assets/BuddyUpLogo.png" alt = "IDK WHY IS THIS IMAGE BROKEN HELP">
+  </div>
+
+  <h1>CREATE AN ACCOUNT</h1>
+
+  <div class = "container">
+    <form id = "userRegistration">
+      <div class = "formli">
+        <label for = "fullName">Full Name: </label>
+        <input type = "text" id = "fullName" required = "" placeholder = "Enter your Full Name"> <br><br>
+
+        <label for = "username">Username: </label>
+        <input type = "text" id = "username" required = "" placeholder = "Enter your Username"> <br><br>
+
+        <label for = "emailAddress">Email Address: </label>
+        <input type = "email" id = "emailAddress" required = "" placeholder = "Enter your Email"> <br><br>
+
+        <label for = "password">Password: </label>
+        <input type = "password" id = "password" required = "" placeholder = "Enter your Password"> <br><br>
+
+        <label for = "repeatPassword">Repeat Password: </label>
+        <input type = "password" id = "repeatPassword" required = "" placeholder = "Enter Password again"> <br><br>
+
+        <div class = "signUp">
+          <button id = "signUpButton" type="button" v-on:click="checkValidityOfForm()">Sign Up</button>
+        </div>  
+
+      </div>
+    </form>
+    <div id="loginPromptRouter">
+      <router-link to="/login">Have an account? Login here!</router-link>
+      <router-view/>
+    </div>
+  </div>
 </template>
 
 <script>
+import firebaseApp from '../firebase.js';
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
+const db = getFirestore(firebaseApp);
+
 export default {
 
+  methods: {
+    checkValidityOfForm() {
+      var userFullName = document.getElementById("fullName").value
+      var userUsername = document.getElementById("username").value
+      var userEmail = document.getElementById("emailAddress").value
+      var userPassword = document.getElementById("password").value
+      var userRepeatPassword = document.getElementById("repeatPassword").value
+      if (userFullName == "") {
+        alert("Please key in your Full Name");
+      } else if (userUsername == "") {
+        alert("Please key in a username");
+      } else if (userEmail == "") {
+        alert("Please key in your Email Address")
+      } else if (userPassword == "") {
+        alert("Please key in a password");
+      } else if (userRepeatPassword == "") {
+        alert("Please key in your password again")
+      } else if (userPassword != userRepeatPassword) {
+        alert("Your passwords are not the same. Please key in your passwords again.")
+      } else {
+        alert("We will be registering you: " + userFullName)
+        try {
+          const docRef = addDoc(collection(db, "Users"), {
+            fullName: userFullName, username: userUsername, emailAddress: userEmail
+          })
+          console.log(docRef)
+          document.getElementById('userRegistration').reset();
+          // this.$emit("added")
+        }
+        catch(error) {
+          console.error("Error adding document: ", error);
+        }
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
 
+label {
+  display: inline-block;
+  width: 150px;
+  margin-right: 5px;
+  text-align: right;
+}
+
+input {
+  size: 10;
+}
+
+#signUpButton {
+  background-color: #F07575;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+#nav {
+  padding: 30px
+}
 </style>
