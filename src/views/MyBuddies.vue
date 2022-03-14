8dff89a40b588314bf1ceee287cd4f7c918b0927
Milton <mltnsia@gmail.com>
2022-03-14 14:32:45 +0800

    secured pages

--- src/views/MyBuddies.vue
+++ src/views/MyBuddies.vue
@@ -1,51 +1,59 @@
 <template>
-  <NavBar />
-  <h1>My Buddies Page</h1>
-  <br />
-  <div class="row">
-    <div class="column">
-      <img
-        id="icon"
-        src="@/assets/buddyicon.png"
-        width="150"
-        alt="Buddy Icon"
-      />
-      <br /><br /><router-link to="/indivbuddies/1">Buddy 1</router-link>
-    </div>
-
-    <div class="column">
-      <img
-        id="icon2"
-        src="@/assets/buddyicon.png"
-        width="150"
-        alt="Buddy Icon"
-      />
-      <br /><br />
-      <router-link to="/indivbuddies/2">Buddy 2</router-link>
-    </div>
-
-    <div class="column">
-      <img
-        id="icon3"
-        src="@/assets/buddyicon.png"
-        width="150"
-        alt="Buddy Icon"
-      />
-      <br /><br />
-      <router-link to="/indivbuddies/2">Buddy 2</router-link>
-    </div>
-  </div>
-
-  <div class="row">
+  <div style="text-align: center" v-if="user">
+    <NavBar />
+    <h1>My Buddies Page</h1>
     <br />
-    <button id="addbutton" type="button" v-on:click="addbuddy()">
-      Add a buddy
-    </button>
+    <div class="row">
+      <div class="column">
+        <img
+          id="icon"
+          src="@/assets/buddyicon.png"
+          width="150"
+          alt="Buddy Icon"
+        />
+        <br /><br /><router-link to="/indivbuddies/1">Buddy 1</router-link>
+      </div>
+
+      <div class="column">
+        <img
+          id="icon2"
+          src="@/assets/buddyicon.png"
+          width="150"
+          alt="Buddy Icon"
+        />
+        <br /><br />
+        <router-link to="/indivbuddies/2">Buddy 2</router-link>
+      </div>
+
+      <div class="column">
+        <img
+          id="icon3"
+          src="@/assets/buddyicon.png"
+          width="150"
+          alt="Buddy Icon"
+        />
+        <br /><br />
+        <router-link to="/indivbuddies/2">Buddy 2</router-link>
+      </div>
+    </div>
+
+    <div class="row">
+      <br />
+      <button id="addbutton" type="button" v-on:click="addbuddy()">
+        Add a buddy
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from "../components/NavBar.vue";
+// import firebaseApp from "../firebase.js";
+// import { getFirestore } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+// const db = getFirestore(firebaseApp);
+
 export default {
   components: {
     NavBar,
@@ -56,6 +64,21 @@ export default {
       var image = document.getElementById("icon");
       image.src = "@/assets/buddyicon.png";
     },
+  },
+
+  data() {
+    return {
+      user: false,
+    };
+  },
+
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
+      }
+    });
   },
 };
 </script>
--- src/views/MyCalendar.vue
+++ src/views/MyCalendar.vue
@@ -1,18 +1,35 @@
 <template>
-  <NavBar />
-  <h1>My Calendar Page</h1>
+  <div style="text-align: center" v-if="user">
+    <NavBar />
+    <h1>My Calendar Page</h1>
+  </div>
 </template>
 
 <script>
-import NavBar from '../components/NavBar.vue'
+import NavBar from "../components/NavBar.vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 export default {
   components: {
-    NavBar
-  }
-}
+    NavBar,
+  },
 
+  data() {
+    return {
+      user: false,
+    };
+  },
+
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
+      }
+    });
+  },
+};
 </script>
 
 <style>
-
 </style>
