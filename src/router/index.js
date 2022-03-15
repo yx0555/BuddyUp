8158cbb5d2989d2be882142146f57c4b385df6ab
Milton <mltnsia@gmail.com>
2022-03-15 11:34:06 +0800

    not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,59 +1,65 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from '@/views/Home.vue'
-import Login from '@/views/Login.vue'
-import MyBuddies from '@/views/MyBuddies.vue'
-import MyCalendar from '@/views/MyCalendar.vue'
-import Profile from '@/views/Profile.vue'
-import Register from '@/views/Register.vue'
-import AboutUs from '@/views/AboutUs.vue'
-import IndivBuddies from '@/views/IndivBuddies.vue'
+import Home from "@/views/Home.vue";
+import Login from "@/views/Login.vue";
+import MyBuddies from "@/views/MyBuddies.vue";
+import MyCalendar from "@/views/MyCalendar.vue";
+import Profile from "@/views/Profile.vue";
+import Register from "@/views/Register.vue";
+import AboutUs from "@/views/AboutUs.vue";
+import IndivBuddies from "@/views/IndivBuddies.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const routes = [
-    {
-        path: '/login',
-        name: 'Login',
-        component: Login
-    },
-    {
-        path: '/home',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/mybuddies',
-        name: 'MyBuddies',
-        component: MyBuddies
-    },
-    {
-        path: '/mycalendar',
-        name: 'MyCalendar',
-        component: MyCalendar
-    },
-    {
-        path: '/profile',
-        name: 'Profile',
-        component: Profile
-    },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    }, 
-    {
-        path: '/aboutus',
-        name: 'AboutUs',
-        component: AboutUs
-    },
-    {
-        path: '/indivbuddies/:id',
-        name: 'IndivBuddies',
-        component: IndivBuddies
-    }
-]
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/mybuddies",
+    name: "MyBuddies",
+    component: MyBuddies,
+  },
+  {
+    path: "/mycalendar",
+    name: "MyCalendar",
+    component: MyCalendar,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/aboutus",
+    name: "AboutUs",
+    component: AboutUs,
+  },
+  {
+    path: "/indivbuddies/:id",
+    name: "IndivBuddies",
+    component: IndivBuddies,
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
