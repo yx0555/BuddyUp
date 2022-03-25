e8185dab972179a50fe10315008f25c100d448bc
Milton <mltnsia@gmail.com>
2022-03-25 20:02:23 +0800

    my buddies update

--- src/views/MyBuddies.vue
+++ src/views/MyBuddies.vue
@@ -60,6 +60,7 @@ import {
   collection,
   query,
   doc,
+  updateDoc,
 } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 const db = getFirestore(firebaseApp);
@@ -79,13 +80,94 @@ export default {
       const snapshot = await getDoc(doc(db, "Users", uid));
       var genderPref = snapshot.data().genderPref;
       var region = snapshot.data().region;
+      var languageArray = snapshot.data().languages;
+
+      var buddy1 = snapshot.data().buddyID1;
+      var buddy2 = snapshot.data().buddyID2;
+      var buddy3 = snapshot.data().buddyID3;
+
+      var x;
+      if (buddy1 == "") {
+        x = 1;
+      } else if (buddy2 == "") {
+        x = 2;
+      } else if (buddy3 == "") {
+        x = 3;
+      } else {
+        alert("Maximum number of buddies reached");
+      }
+
+      var available = false;
 
       const querySnap = await getDocs(query(collection(db, "Buddies")));
-      querySnap.forEach((doc) => {
-        if (doc.data().gender == genderPref && doc.data().region == region) {
-          document.getElementById("buddy1").innerHTML = doc.data().buddyName;
+      try {
+        querySnap.forEach((d) => {
+          if (
+            d.data().gender == genderPref &&
+            d.data().region == region &&
+            d.data().userID == "" &&
+            languageArray.includes(d.data().language) &&
+            x == 1
+          ) {
+            console.log(d.id);
+            updateDoc(doc(db, "Buddies", d.id), {
+              userID: uid,
+            });
+            updateDoc(doc(db, "Users", uid), {
+              buddyID1: d.id,
+              buddyName1: d.data().buddyName,
+            });
+            document.getElementById("buddy1").innerHTML = d.data().buddyName;
+            alert(d.data().buddyName + " is your new buddy");
+            available = true;
+            throw "Break";
+          } else if (
+            d.data().gender == genderPref &&
+            d.data().region == region &&
+            d.data().userID == "" &&
+            languageArray.includes(d.data().language) &&
+            x == 2
+          ) {
+            console.log(d.id);
+            updateDoc(doc(db, "Buddies", d.id), {
+              userID: uid,
+            });
+            updateDoc(doc(db, "Users", uid), {
+              buddyID2: d.id,
+              buddyName2: d.data().buddyName,
+            });
+            document.getElementById("buddy2").innerHTML = d.data().buddyName;
+            alert(d.data().buddyName + " is your new buddy");
+            available = true;
+            throw "Break";
+          } else if (
+            d.data().gender == genderPref &&
+            d.data().region == region &&
+            d.data().userID == "" &&
+            languageArray.includes(d.data().language) &&
+            x == 3
+          ) {
+            console.log(d.id);
+            updateDoc(doc(db, "Buddies", d.id), {
+              userID: uid,
+            });
+            updateDoc(doc(db, "Users", uid), {
+              buddyID3: d.id,
+              buddyName3: d.data().buddyName,
+            });
+            document.getElementById("buddy3").innerHTML = d.data().buddyName;
+            alert(d.data().buddyName + " is your new buddy");
+            available = true;
+            throw "Break";
+          }
+        });
+      } catch (e) {
+        if (e !== "Break") throw e;
+      } finally {
+        if (available == false) {
+          alert("No Buddies Available");
         }
-      });
+      }
     },
   },
 
@@ -101,8 +183,8 @@ export default {
       if (user) {
         this.user = user;
       } else {
-        alert("you must be logged in to view this page")
-        this.$router.push("/")
+        alert("you must be logged in to view this page");
+        this.$router.push("/");
       }
     });
   },
@@ -121,14 +203,14 @@ export default {
   color: antiquewhite;
 }
 
-.column{
+.column {
   font-family: "Montserrat";
   color: #000000;
 }
 
 #addbutton {
   font-family: "Montserrat";
-  background-color: #ABE6E9;
+  background-color: #abe6e9;
   font-size: 15px;
   font-weight: 500;
   border-radius: 5px;
