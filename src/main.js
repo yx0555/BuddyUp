171e82da70302962cdc57804486010b000647fe5
melissaoyl <yilinmelissa@gmail.com>
2022-03-28 20:53:15 +0800

    replaced calendar

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
-import { SetupCalendar, Calendar, DatePicker} from 'v-calendar';
+// import { SetupCalendar } from 'v-calendar';
 
 createApp(App).use(router).mount('#app')
 
-const app = createApp(App)
-app.use(SetupCalendar, {})
-app.component('Calendar', Calendar)
-app.component('DatePicker', DatePicker)
+// const app = createApp(App)
+// app.use(SetupCalendar, {})
+// app.component('Calendar', Calendar)
+// app.component('DatePicker', DatePicker)
 
 // new App({
 //     router,
